1378d0850780976885d9bf74c484262bf76d888e
Juan Cruz Gasparro <gasparrojuancruz@gmail.com>
2024-01-04 19:42:46 -0300

    setup index.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'todo',
+    loadChildren: () => import('./todo/todo.module').then((m) => m.TodoModule),
+  },
+  {
+    path: '',
+    redirectTo: '/todo',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/todo/components/todo-index/todo-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-index.component.html',
+  styleUrl: './todo-index.component.css'
+})
+export class TodoIndexComponent {
+
+}
