39861e35bcbfcfc078603cd75f791ca7bddf24e9
Juan Cruz Gasparro <gasparrojuancruz@gmail.com>
2024-01-05 22:59:47 -0300

    basic styles.

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -3,12 +3,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const modules = [
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
   MatListModule,
+  MatInputModule,
+  MatFormFieldModule,
 ];
 
 @NgModule({
--- src/app/todo/todo-index/todo-index.component.ts
+++ src/app/todo/todo-index/todo-index.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { Todo } from '@shared/models/todo.model';
 import { MaterialModule } from '@shared/modules/material.module';
 import { AddTodo, ToggleCompleteTodo } from '@shared/state/todo/todo.actions';
@@ -11,11 +12,13 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-todo-index',
   standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FormsModule],
   templateUrl: './todo-index.component.html',
   styleUrl: './todo-index.component.css',
 })
 export class TodoIndexComponent implements OnInit {
+  public todoDescription = '';
+
   public todoEntities$!: Observable<Todo[]>;
 
   constructor(private _store: Store<ITodoState>) {}
@@ -24,9 +27,12 @@ export class TodoIndexComponent implements OnInit {
     this.todoEntities$ = this._store.select(selectTodoEntities);
   }
 
-  addTodoHandler(description: string): void {
-    const todo = new Todo({ description });
-    this._store.dispatch(AddTodo({ todo }));
+  addTodoHandler(): void {
+    if (!this.todoDescription) return;
+    this._store.dispatch(
+      AddTodo({ todo: new Todo({ description: this.todoDescription }) })
+    );
+    this.todoDescription = '';
   }
 
   toggleTodoCompleteHandler(id?: string): void {
